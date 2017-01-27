3920cc892f126d7642a9a7a4c966844a287f6087
Pouyan <pouyank13@me.com>
2017-01-27 08:48:27 -0700

    different approach just getting array factor plotted for N*M. going to branch to clean things up

--- compensation_error_analysis.m
+++ compensation_error_analysis.m
@@ -14,35 +14,39 @@ phi = linspace(0,2*pi,500);
 fo = 79e9;
 c = 2.99792458e8;                                           % frequency of 
 lamda = c/fo;                                               % free space wavelength
-d = 0.5*lamda;                                              % element seperation
+d = 0.8*lamda;                                              % element seperation
 theta_i = pi/4;                                             % incident angle on array
-i_phi = 2*pi/lamda*d*cos(theta_i);                          % relative incident phases
+%i_phi = 2*pi/lamda*d*cos(theta);                            % relative incident phases
 M = 4;                                                      % Number of subarray
 N = 4;                                                      % Number of elements per sub array
-delta_phi_1 = N*(M-1)*i_phi;                                % Compensation values for farthest array
-delta_phi = linspace(N*i_phi,delta_phi_1,M/2);              % Vector of compensation values for each subarray  
-i_phi_vector = 0:-i_phi:-(M*N-1)*i_phi;                     % relative incident phases for all elements
-i_phi_vector = wrapTo2Pi(i_phi_vector);                         % normalize to 2*pi rad 
+% for every elemt
+
+%delta_phi_1 = N*(M-1)*i_phi;                                % Compensation values for farthest array
+%delta_phi = linspace(N*i_phi,delta_phi_1,M/2);              % Vector of compensation values for each subarray  
+%i_phi_vector = 0:-i_phi:-(M*N-1)*i_phi;                     % relative incident phases for all elements
+%i_phi_vector = wrapTo2Pi(i_phi_vector);                         % normalize to 2*pi rad 
 
 %% Prior to compensating analysis
 % Plot AF for each array
 % Plot total AF
 close all
 clc
-
-for i = 1:size(i_phi_vector,2)
-    for t = 1:size(theta,2)
-        AF_values(i,t) = exp(1i*(2*pi/lamda*d*cos(theta(t)) - i_phi_vector(i)));
+for n = 0:N*M-1
+    for i = 1:size(theta,2)
+        AF_values(n+1,i) = exp(-1i*n*2*pi/lamda*d*cos(theta(i)));
     end
 end
 
-%%
 %AF_values_mag = abs(AF_values);
+AF_total = sum(AF_values(1:16,:));
 AF_element1 = sum(AF_values(1:4,:));
 AF_element2 = sum(AF_values(5:8,:));
 AF_element3 = sum(AF_values(9:12,:));
 AF_element4 = sum(AF_values(13:16,:));
+figure(1)
+polar(theta, AF_total);
 
+%%
 AF_total = [AF_element1; AF_element2; AF_element3; AF_element4];
 for i = 1:M
     figure(1)
